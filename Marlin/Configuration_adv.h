bb1865defcc774eebe45057c174a35fb73e78c64
Evg33 <evg33@evg33.ru>
2019-10-12 00:25:24 +0300

    evg: XY-TMC5160, ZE-TMC2209

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1809,8 +1809,8 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT     900  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_MICROSTEPS   32  // 0..256
-    //#define X_RSENSE     0.075
-    #define X_RSENSE     0.11
+    #define X_RSENSE     0.075  //5160
+    //#define X_RSENSE     0.11  //2209
     #define X_CHAIN_POS    -1  // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
 
@@ -1824,8 +1824,8 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT     900
     #define Y_MICROSTEPS   32
-    //#define Y_RSENSE     0.075
-    #define Y_RSENSE     0.11
+    #define Y_RSENSE     0.075  //5160
+    //#define Y_RSENSE     0.11  //2209
     #define Y_CHAIN_POS    -1
   #endif
 
