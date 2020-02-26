8e4119c684ee9e48789de984fb55464b65a70bf8
Evg33 <evg33@evg33.ru>
2020-02-27 01:19:58 +0300

    evg: BABYSTEPPING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1003,7 +1003,7 @@
  * Specify a Probe position as { X, Y, Z }
  */
 //#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.3 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.16 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
