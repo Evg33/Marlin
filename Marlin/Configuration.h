c1c49a7611bc8581b448c8e961649811c52e87f9
Evg33 <evg33@evg33.ru>
2020-02-28 03:21:20 +0300

    evg: PID bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -556,10 +556,14 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // UNI M303 E-1 C3 S70 U1    ///  M303 E-1 C8 S100 U1 /// M303 E-1 C8 S100 U1
-  #define DEFAULT_bedKp 38.36
-  #define DEFAULT_bedKi 4.56
-  #define DEFAULT_bedKd 215.04
-  // UNI 200228 M303 E-1 C8 S80 U1
+  //#define DEFAULT_bedKp 38.36
+  //#define DEFAULT_bedKi 4.56
+  //#define DEFAULT_bedKd 215.04
+  // UNI 200228 M303 E-1 C8 S75 U1 (83 max)
+  #define DEFAULT_bedKp 46.97
+  #define DEFAULT_bedKi 7.63
+  #define DEFAULT_bedKd 192.90
+
   
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
