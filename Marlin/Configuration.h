cd42a2eeabd1e98cfa498b30c6e71b8890f1ded7
Evg33 <evg33@evg33.ru>
2020-04-24 23:50:21 +0300

    evg: spd/acc tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -812,7 +812,7 @@
 //#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
 //#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }
 //#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 2000 } //Lerdge defaults
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 5000, 5000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -831,9 +831,9 @@
 //#define DEFAULT_ACCELERATION          1200    // X, Y, Z and E acceleration for printing moves
 //#define DEFAULT_RETRACT_ACCELERATION  1500    // E acceleration for retracts
 //#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
-#define DEFAULT_ACCELERATION          2500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          2800    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  4000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   4000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
