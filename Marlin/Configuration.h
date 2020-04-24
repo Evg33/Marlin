1a05b617e1aeec6be7d587640eac76af8ff90b31
Evg33 <evg33@evg33.ru>
2020-04-24 04:09:37 +0300

    evg: UMI NEOPIXEL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2288,22 +2288,22 @@
 #endif
 
 // Support for Adafruit Neopixel LED driver
-//#define NEOPIXEL_LED
+#define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   //#define NEOPIXEL_TYPE   NEO_GRBW // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
-  #define NEOPIXEL_TYPE   NEO_RGB // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
+  #define NEOPIXEL_TYPE   NEO_GRB // NEO_GRBW / NEO_GRB / NEO_RGB- four/three channel driver type (defined in Adafruit_NeoPixel.h)
   //#define NEOPIXEL_PIN     P1_21  // LED driving pin
   //#define NEOPIXEL2_TYPE NEO_GRB
   //#define NEOPIXEL2_PIN P2_00
-  #define NEOPIXEL_PIXELS 3       // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
+  #define NEOPIXEL_PIXELS 8       // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
   #define NEOPIXEL_IS_SEQUENTIAL   // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
-  #define NEOPIXEL_BRIGHTNESS 96  // Initial brightness (0-255)
+  #define NEOPIXEL_BRIGHTNESS 7  // Initial brightness (0-255)
   //#define NEOPIXEL_STARTUP_TEST // Cycle through colors at startup
 
   // Use a single Neopixel LED for static (background) lighting
-  #define NEOPIXEL_BKGD_LED_INDEX  0               // Index of the LED to use //0-sc, 1-l,2-r
+  //#define NEOPIXEL_BKGD_LED_INDEX  0               // Index of the LED to use //0-sc, 1-l,2-r
   //#define NEOPIXEL_BKGD_COLOR { 255, 255, 255, 0 } // R, G, B, W
-  #define NEOPIXEL_BKGD_COLOR { 0, 0, 255, 0 } // R, G, B, W
+  //#define NEOPIXEL_BKGD_COLOR { 0, 0, 255, 0 } // R, G, B, W
 #endif
 
 /**
