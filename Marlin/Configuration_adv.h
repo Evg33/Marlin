b182eade2e5782f5eb7701e4bdfb2cfe690d95a5
Evg33 <evg33@evg33.ru>
2020-04-27 16:33:37 +0300

    evg: disable STARTUP_COMMANDS "M22"

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3018,7 +3018,7 @@
  * Execute certain G-code commands immediately after power-on.
  */
 //#define STARTUP_COMMANDS "M17 Z"
-#define STARTUP_COMMANDS "M22"
+//#define STARTUP_COMMANDS "M22"
 
 /**
  * G-code Macros
