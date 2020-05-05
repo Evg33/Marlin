b1d78cbea4273dcf68f95e45bd85058684fe91e2
Evg33 <evg33@evg33.ru>
2020-05-05 21:55:32 +0300

    evg: Y_BED_SIZE 158

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1179,7 +1179,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 160
-#define Y_BED_SIZE 159
+#define Y_BED_SIZE 158
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
