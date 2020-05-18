11debd2e67a55edcde0828e6aad3d290fcec3911
Evg33 <evg33@evg33.ru>
2020-05-18 03:13:34 +0300

    evg: FYSETC_MINI_12864_1_2

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1005,11 +1005,11 @@
     #define LED_COLOR_PRESETS                 // Enable the Preset Color menu option
     #if ENABLED(LED_COLOR_PRESETS)
       #define LED_USER_PRESET_RED          0  // User defined RED value
-      #define LED_USER_PRESET_GREEN        0  // User defined GREEN value
-      #define LED_USER_PRESET_BLUE        64  // User defined BLUE value
-      #define LED_USER_PRESET_WHITE      128  // User defined WHITE value
-      #define LED_USER_PRESET_BRIGHTNESS 128  // User defined intensity
-      //#define LED_USER_PRESET_STARTUP       // Have the printer display the user preset color on startup
+      #define LED_USER_PRESET_GREEN      255  // User defined GREEN value
+      #define LED_USER_PRESET_BLUE         0  // User defined BLUE value
+      #define LED_USER_PRESET_WHITE        0  // User defined WHITE value
+      #define LED_USER_PRESET_BRIGHTNESS   5  // User defined intensity
+      #define LED_USER_PRESET_STARTUP       // Have the printer display the user preset color on startup
     #endif
   #endif
 
