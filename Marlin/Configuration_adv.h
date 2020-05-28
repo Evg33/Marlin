d2bec58b0e976e934583e4f2477f718567f1f82c
Evg33 <evg33@evg33.ru>
2020-05-28 21:58:21 +0300

    evg: XY_HYBRID >200

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2405,9 +2405,9 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     90 //105  // [mm/s]
+  #define X_HYBRID_THRESHOLD     200 //90 //105  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     90 //105
+  #define Y_HYBRID_THRESHOLD     200 //90 //105
   #define Y2_HYBRID_THRESHOLD    100
   #define Z_HYBRID_THRESHOLD      50 //12
   #define Z2_HYBRID_THRESHOLD      3
