8331e11315b5f2ef60ddeba1c48740e32c59f4d4
Evg33 <evg33@evg33.ru>
2020-06-10 23:10:23 +0300

    evg: FYSETC2.1->RepRapFG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1784,7 +1784,7 @@
 // produce one step. Should be increased for high-resolution encoders.
 //
 //#define ENCODER_PULSES_PER_STEP 4
-#define ENCODER_PULSES_PER_STEP 3 //Fysetc mini 12864
+//#define ENCODER_PULSES_PER_STEP 3 //Fysetc mini 12864
 
 //
 // Use this option to override the number of step signals required to
@@ -2001,7 +2001,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
@@ -2071,11 +2071,11 @@
 //#define FYSETC_MINI_12864_X_X    // Type C/D/E/F. No tunable RGB Backlight by default
 //#define FYSETC_MINI_12864_1_2    // Type C/D/E/F. Simple RGB Backlight (always on)
 //#define FYSETC_MINI_12864_2_0    // Type A/B. Discreet RGB Backlight
-#define FYSETC_MINI_12864_2_1    // Type A/B. Neopixel RGB Backlight
+//#define FYSETC_MINI_12864_2_1    // Type A/B. Neopixel RGB Backlight
 //#define FYSETC_GENERIC_12864_1_1 // Larger display with basic ON/OFF backlight.
-#if ENABLED(FYSETC_MINI_12864_2_1)
- #define LCD_CONTRAST_INIT 230
-#endif
+//#if ENABLED(FYSETC_MINI_12864_2_1)
+ //#define LCD_CONTRAST_INIT 230
+//#endif
 
 //
 // Factory display for Creality CR-10
