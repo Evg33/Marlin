6b67bc38fda3b7dd1e187276436e8c93de581652
Evg33 <evg33@evg33.ru>
2020-07-16 22:40:16 +0300

    evg: CLASSIC_JERK

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -870,7 +870,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
@@ -881,7 +881,7 @@
 
   //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
   #if ENABLED(LIMITED_JERK_EDITING)
-    #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
+    #define MAX_JERK_EDIT_VALUES { 40, 40, 0.8, 15 } // ...or, set your own edit limits
   #endif
 #endif
 
