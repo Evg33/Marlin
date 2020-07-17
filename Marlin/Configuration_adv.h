1cbc787bf3019a930445df7d3e655f28e860e39e
Evg33 <evg33@evg33.ru>
2020-07-17 12:52:04 +0300

    evg: CHOPPER_09STEP_24V

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2442,7 +2442,7 @@
    * { <off_time[1..15]>, <hysteresis_end[-3..12]>, hysteresis_start[1..8] }
    */
   //#define CHOPPER_TIMING CHOPPER_DEFAULT_24V
-  #define CHOPPER_TIMING { 3, -1, 5 } //CHOPPER_09STEP_24V
+  #define CHOPPER_TIMING CHOPPER_09STEP_24V
 
   /**
    * Monitor Trinamic drivers
